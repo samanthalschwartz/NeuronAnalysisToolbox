a79514fd759b95faf5340b710f8dee1df101f57a
ss <samantha.schwartz@ucdenver.edu>
2018-06-06 20:50:12 -0600

    added to plotting function for plotting histograms

--- Ashley's Scripts/makinghistogramPlots.m
+++ Ashley's Scripts/makinghistogramPlots.m
@@ -1,55 +1,97 @@
 %% --- 
 % ---------------update these values!!!---------------
-load('Z:\Ashley\For Sam\050118 NL1 insertion\cell3_AshleyFile\MinPaths.mat');
+% datapath ='G:\Sam\Data\Ashley\050118 NL1 insertion';
+% datafiles = {'cell1_AshleyFile','cell3_AshleyFile','cell4_AshleyFile','cell5_AshleyFile'};
+datapath = 'G:\Sam\Data\Ashley\DHFR-GFP-GluA1\051718\merges\slip1';
+datafiles = {'1_merge_AshleyFile','2_merge_AshleyFile','3_merge_AshleyFile','4_merge_AshleyFile','5_merge_AshleyFile'};
+% load(datafilename);
 % datafilename = 'Z:\Ashley\For Sam\051318 NL1 insertion\7_merge_AshleyFile';
 % load(fullfile(datafilename,'MinPaths.mat'));
 px2um = 0.2857;
 % glua1 info
-baseline_frames = (1:6)-1;
-baseline_minperframe = 2;
+baseline_frames = 1:(11-1);
+baseline_minperframe = 1;
 postrelease_minperframe = 2;
-% nrl1 info
-% baseline_frames = (1:(6-1));
-% baseline_minperframe = 2;
-% ------------- clean up the data ------------------
-% first copy the distMat to clean things up and convert
-distVtimemat = distMat;
-% for now get rid of frame = 0 and frame = end
-distVtimemat(distVtimemat(:,1) == 0,:) = [];
-distVtimemat(distVtimemat(:,1) == max(distVtimemat(:,1)),:) = [];
-distVtimemat(isnan(distVtimemat(:,1)),:) = [];
-%convert to microns
-distVtimemat(:,2) = distVtimemat(:,2) .* px2um;
-%-- convert baseline and postrelease to minutes
-baselineids = ismember(distVtimemat(:,1),baseline_frames);
-postreleaseids = distVtimemat(:,1)>= (baseline_frames(end)+1);
-distVtimemat(baselineids,1) = distVtimemat(baselineids,1) .* baseline_minperframe;
-distVtimemat(postreleaseids,1) = distVtimemat(postreleaseids,1) .* postrelease_minperframe;
-max(distVtimemat(:,2))
-%% 
-%  now you want distVtimemat to represent your results
-
-%% ------------- now we can plot some things -----------------
-% break cargo appearances up into times
 distances = [0:20:100];
-bins = zeros(numel(distances),1);
-f = figure; cax = gca;
+
+savedhistograms = cell(numel(datafiles),numel(distances));
+for ff = 1:numel(datafiles)
+curr = load(fullfile(datapath,datafiles{ff},'MinPaths.mat'));
+%% to get just post release time course
+% %  now you want distVtimemat to represent your results
+% % postrelease_mat = distVtimemat(distVtimemat(:,1)>baseline_frames(end),:);
+
+postreleaseids = curr.distMat(:,1)>= (baseline_frames(end)+1);
+postrelease_mat = curr.distMat(postreleaseids,:);
+% for now get rid of frame = 0 and frame = end
+postrelease_mat(postrelease_mat(:,1) == max(postrelease_mat(:,1)),:) = [];
+postrelease_mat(isnan(postrelease_mat(:,1)),:) = [];
+%convert to microns
+postrelease_mat(:,2) = postrelease_mat(:,2) .* px2um;
+%convert to mins
+postrelease_mat(:,1) = postrelease_mat(:,1) .* postrelease_minperframe;
+%% loop at save histogram objects for each time group
 for tt = 1:numel(distances)
     if tt < numel(distances)
-        binvals = distVtimemat(distVtimemat(:,2)>= distances(tt) & distVtimemat(:,2)< distances(tt+1),:);
-        titlestr = (['Events Occuring between '...
-            num2str(distances(tt)) ' - ' num2str(distances(tt+1)) ' \mum of Cell Soma']);
-  
+        binvals = postrelease_mat(postrelease_mat(:,2)>= distances(tt) & postrelease_mat(:,2)< distances(tt+1),:);  
     else
-        binvals = distVtimemat(distVtimemat(:,2)>= distances(tt),:);
-        titlestr = (['Events Occuring at greater than '...
-            num2str(distances(tt)) ' \mum of Cell Soma']);
+        binvals = postrelease_mat(postrelease_mat(:,2)>= distances(tt),:);
     end
-    subplot(numel(distances),1,tt)
-    histo = histogram(binvals(:,1),10);
-    title(titlestr,'FontSize',12)
-    xlabel('Time (min)','FontSize',10);
-    ylabel('# Cargo Events','FontSize',10);
+    histo = histogram(binvals(:,1),'BinEdges',distances); 
+    info.xvals = diff(histo.BinEdges)/2 + histo.BinEdges(1:end-1);
+    info.yvals = histo.Values./size(histo.Data,1); % normalize to total events
+    savedhistograms{ff,tt} = info;
 end
+end
+
+%% make the plot
+f = figure; hold on;
+for tt = 1:numel(distances)
+    subplot(numel(distances),1,tt); hold on;
+    for ff = 1:numel(datafiles)
+        info = savedhistograms{ff,tt};
+        plot(info.xvals,info.yvals');
+    end
+end
+%%
 % suptitle(['Cargo Delivery For File: ' datafilename])
 
+% %% ------------- now we can plot some things -----------------
+% % break cargo appearances up into times
+% 
+% bins = zeros(numel(distances),1);
+% f = figure; cax = gca;
+% for tt = 1:numel(distances)
+%     if tt < numel(distances)
+%         binvals = distVtimemat(distVtimemat(:,2)>= distances(tt) & distVtimemat(:,2)< distances(tt+1),:);
+%         titlestr = (['Events Occuring between '...
+%             num2str(distances(tt)) ' - ' num2str(distances(tt+1)) ' \mum of Cell Soma']);
+%     else
+%         binvals = distVtimemat(distVtimemat(:,2)>= distances(tt),:);
+%         titlestr = (['Events Occuring at greater than '...
+%             num2str(distances(tt)) ' \mum of Cell Soma']);
+%     end
+%     subplot(numel(distances),1,tt)
+%     histo = histogram(binvals(:,1),10);
+%     title(titlestr,'FontSize',12)
+%     xlabel('Time (min)','FontSize',10);
+%     ylabel('# Cargo Events','FontSize',10);
+% end
+% suptitle(['Cargo Delivery For File: ' datafilename])
+
+%%
+% %% to get entire time course ------------- clean up the data ------------------
+% % first copy the distMat to clean things up and convert
+% distVtimemat = distMat;
+% % for now get rid of frame = 0 and frame = end
+% distVtimemat(distVtimemat(:,1) == 0,:) = [];
+% distVtimemat(distVtimemat(:,1) == max(distVtimemat(:,1)),:) = [];
+% distVtimemat(isnan(distVtimemat(:,1)),:) = [];
+% %convert to microns
+% distVtimemat(:,2) = distVtimemat(:,2) .* px2um;
+% %-- convert baseline and postrelease to minutes
+% baselineids = ismember(distVtimemat(:,1),baseline_frames);
+% postreleaseids = distVtimemat(:,1)>= (baseline_frames(end)+1);
+% distVtimemat(baselineids,1) = distVtimemat(baselineids,1) .* baseline_minperframe;
+% distVtimemat(postreleaseids,1) = distVtimemat(postreleaseids,1) .* postrelease_minperframe;
+% max(distVtimemat(:,2))
