0bb6530951447f17fb5ab29b957aa8963723ea5c
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-07-11 17:42:36 -0600

    updated some SR analysis

---
+++ SR Analysis/saving2mat.m
@@ -0,0 +1,27 @@
+filedir = 'C:\Users\schwsama\Documents\Data\SR\coverslip4-1a-647sr';
+filess = dir(fullfile(filedir,'*.tif'));
+
+totframes = numel(filess);
+breakdown = 5;
+inc = ceil(totframes/breakdown);
+increments = [1:inc:totframes,totframes];
+movie = [];
+frame1 = loadtiff(fullfile(filedir,filess(1).name));
+
+for bb = 2:numel(increments)
+    movsize = increments(bb)-increments(bb-1);
+movie = zeros(size(frame1,1),size(frame1,2),movsize);
+w = waitbar(0);
+moviecnt = 1;
+for ff = increments(bb-1):increments(bb)
+    frame = loadtiff(fullfile(filedir,filess(ff).name));
+%     frame = GeneralAnalysis.loadtiff_3ch(fullfile(filedir,filess(ff).name));
+    movie(:,:,moviecnt) = squeeze(single(frame(:,:,1)));
+    waitbar(moviecnt/movsize,w);
+    moviecnt = moviecnt+1;
+end
+close(w)
+sequence = dip_image(movie);
+savename = ['C:\Users\schwsama\Documents\Data\SR\coverslip4-1a-647srTry2_' num2str(increments(bb))];
+save(savename,'sequence');
+end
