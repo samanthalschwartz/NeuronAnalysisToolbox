cf0b8d60b28ddb3fef920e5b12a941dc2147ddb5
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-07-12 14:42:52 -0600

    c

--- SR Analysis/saving2mat.m
+++ SR Analysis/saving2mat.m
@@ -16,12 +16,12 @@ moviecnt = 1;
 for ff = increments(bb-1):increments(bb)
     frame = loadtiff(fullfile(filedir,filess(ff).name));
 %     frame = GeneralAnalysis.loadtiff_3ch(fullfile(filedir,filess(ff).name));
-    movie(:,:,moviecnt) = squeeze(single(frame(:,:,1)));
+    movie(:,:,moviecnt) = squeeze(uint16(frame(:,:,1)));
     waitbar(moviecnt/movsize,w);
     moviecnt = moviecnt+1;
 end
 close(w)
-sequence = dip_image(movie);
-savename = ['C:\Users\schwsama\Documents\Data\SR\coverslip4-1a-647srTry2_' num2str(increments(bb))];
+sequence = movie;
+savename = ['C:\Users\schwsama\Documents\Data\SR\coverslip4-1a-647srTry2_' num2str(increments(bb)) '.mat'];
 save(savename,'sequence');
 end
