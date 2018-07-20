d84a92b741170560ff4d6fe1d121e8174bec0662
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-07-20 07:52:08 -0600

    updated

--- InProgress/runningstitching.m
+++ InProgress/runningstitching.m
@@ -7,12 +7,28 @@ soma1 = squeeze(single(soma(:,:,1)));
 dend1 = squeeze(single(dendrites(:,:,1)));
 
 tic
-GeneralAnalysis.stitch2images(soma1,dend1);
+[newimage, ccpeak] = GeneralAnalysis.stitch2images(soma1,dend1);
 toc
 
 soma1 = single(soma);
 dend1 = single(dendrites);
 
 tic
-GeneralAnalysis.stitch2movies(soma1,dend1);
+[stitchmovie1,ccpeak] = GeneralAnalysis.stitch2movies(soma1,dend1);
+toc
+
+
+soma = single(s.aa.TfR.image);
+dendrites = single(d.aa.TfR.image);
+
+tic
+[stitchmovie2,ccpeak] = GeneralAnalysis.stitch2movies(soma,dendrites,ccpeak);
+toc
+
+
+soma2 = single(s.aa.surfaceCargo.image);
+dendrites2 = single(d.aa.surfaceCargo.image);
+
+tic
+[stitchmovie3,ccpeak] = GeneralAnalysis.stitch2movies(soma2,dendrites2,ccpeak);
 toc
