438889a398f7a5974ba987c4d10e4a78983adc30
ss <samantha.schwartz@ucdenver.edu>
2018-08-08 11:23:16 -0600

    updated to just give output Bill wants - then saves info as matlab file for further processing

--- InProgress/BillCalcium_rGecko.m
+++ InProgress/BillCalcium_rGecko.m
@@ -1,13 +1,27 @@
-%% for automatically finding calcium transients and determining the extent of calcium
+%%-- script for automatically finding calcium transients and determining the extent of calcium
+% this will prompt you to select a file to analyze and an excel sheet name
+% to save it as
+%  Make sure the excel file is closed to enable writing to it. Otherwise,
+%  there will be an error.
+%% run this cell to test out thresholding parameter using 'Run Section' button.
+% then once you're happy with the parameter, 'Run' the entire script
+threshold_parameter = 1.3; % smaller number is more sensitive, larger number is less sensitive
 
-% image = GeneralAnalysis.loadtiff_2ch('F:\GephIntrabody Project\Bill rGecko\180806\cell2\postZ_movie.tif');
-image = GeneralAnalysis.loadtiff_2ch('F:\GephIntrabody Project\Bill rGecko\180806\cell2\baseline_movie.tif');
+% Select the file you want to analyze: for now this is a 1 color time
+% series tif file
+excelfileinfo_maxextent = 'E:\Bill\Geph_IB-Cry2Olig-GFP plus blue light\calcium imaging\0 Mg QCTs and dendritic spikes\MaxExtensions.xlsx';
+excelfileinfo_Fvalues = 'E:\Bill\Geph_IB-Cry2Olig-GFP plus blue light\calcium imaging\0 Mg QCTs and dendritic spikes\ROIintensities.xlsx';
 
-ca = dip_image(image(:,:,1:end-1,1));
-
-% uiopen('C:\Users\schwsama\Documents\Data\Hannah Calcium\Abeta1Post.tif',1)
-% ca = image;
-
+startdir = 'E:\Bill\Geph_IB-Cry2Olig-GFP plus blue light\calcium imaging';
+[filename, pathname, filterindex] = uigetfile(fullfile(startdir,'*.tif'));
+prompt = {'Select a sheet name for this cell/dataset'};
+title = 'Input';
+dims = [1 35];
+definput = {filename(1:end-4)};
+sheetname = inputdlg(prompt,title,dims,definput);
+ca = GeneralAnalysis.loadtiff_1ch(fullfile(pathname,filename));
+% if last frame is empty uncomment this line
+ca = ca(:,:,0:end-1);
 % first smooth image
 ca_g = gaussf(ca,[1 1 0]);
 % now normalize to median
@@ -17,81 +31,77 @@ ca_dzz = dzz(ca_gnorm,[2 2 3]);
 % get absolute values
 ab_ca_dzz = abs(ca_dzz);
 gab_ca_dzz = gaussf(ab_ca_dzz,[1 1 0]);
-% tt = threshold(ab_ca_dzz,'otsu');
-
-tt = threshold(ab_ca_dzz^1.3,'otsu');
-% lbl = label(tt,1,200,10^10);
+tt = threshold(ab_ca_dzz^threshold_parameter,'otsu');
 lbl = label(tt,1,20,10^10);
+% this is to view the identified masks over the image
+[h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(ca,bdilation(lbl>0,2));
+%%
+close(h);
 tic;
 msr = measure(lbl,ca,({'size','DimensionsCube'}));
 toc;
-GeneralAnalysis.viewMaskOverlayPerimStatic(ca,bdilation(lbl>0,2));
-
-
-% tic
-% circularities = msr.P2A;
-% toc
-
-%% plot CDF of max extension
 maxsizes  = max(msr.DimensionsCube([1 2],:));
-[F,X] = ecdf(maxsizes);
-figure; plot(X,F); set(gca,'FontSize',16); 
-xlabel('Max Extension (pixels) of Calcium Propagation','FontSize',16);
-ylabel('Cumulative Distribution','FontSize',16);
-
+% now get this setup to write to excel file
+if isempty(sheetname)
+sheetname = filename(1:end-4);
+else 
+    sheetname = sheetname{1};
+end
 %% plot heatmap of intensity within ROI over time
 
 %label sum projection
 slbl = sum(lbl,[],3);
 sumlbl = label(slbl>0);
-trace_raw = zeros(max(lbl),size(ca,3));
+trace_raw = zeros(max(sumlbl),size(ca,3));
 wb = waitbar(0,'Quantifying Calcium Change in ROIs...');
-for ll = 1:max(sumlbl)%[1:8,10:max(lbl)]
+for ll = 1:max(sumlbl)
 %     tic
 currmask = sumlbl==ll;
-% bcurrmask = bdilation(currmask,1);
 mask2use = repmat(currmask,1,1,size(ca,3));
 sumval = sum(ca,mask2use,[1 2]);
-% imgINmask = image*mask2use;
-% sumval = sum(imgINmask,[],[1 2]);
-% sizeval = sum(bcurrmask,[],[1,2]);
-% tracemed = median(sumval);
-% trace(ll,:) = sumval./tracemed;
 trace_raw(ll,:) = sumval;
 % toc
 waitbar(ll/max(sumlbl),wb);
 end
 close(wb)
 
-
 % plot in order of roi max extension
 tic;
 msrAll = measure(sumlbl,ca(:,:,1),({'P2A','size','DimensionsCube'}));
 toc;
-
 spanval = max(msrAll.DimensionsCube([1 2],:));
 [~, ordx] = sort(spanval, 'ascend');
 ord_trace = trace_raw(ordx,:);
-% now plot
-h = msgbox('Plotting HeatMap....');
-times = 1:size(ord_trace,2);
-ord_trace_norm = ord_trace./median(ord_trace,2);
-figure;
-% wb = waitbar(0,'Plotting some things...');
-% cnt = 0;
-for ii = 1:size(ord_trace_norm,1)
-    for jj = 1:size(ord_trace_norm,2)
-        p = patch([times(jj),times(jj)+1,times(jj)+1,times(jj)],[ii-1, ii-1, ii, ii],ord_trace_norm(ii,jj));
-        set(p,'FaceColor','flat','EdgeColor','none');
-        
-    end
-end
-title('rGecko Intensity (AU)','FontSize',16)
-xlabel('Frame','FontSize',16);
-yticks(1:size(ord_trace,1));
-yticklabels(num2str(round(spanval(ordx),2)'))
-ylabel('Span Value (pixels)','FontSize',16)
-xlim([0 size(ord_trace,2)]);
-ylim([0 size(ord_trace,1)]);
-c = colorbar;
-close(h);
+% tt = table(ord_trace(1:10,1:10))
+%% now plot
+% h = msgbox('Plotting HeatMap....');
+% times = 1:size(ord_trace,2);
+% ord_trace_norm = ord_trace./median(ord_trace,2);
+% g = figure;
+% % wb = waitbar(0,'Plotting some things...');
+% % cnt = 0;
+% for ii = 1:size(ord_trace_norm,1)
+%     for jj = 1:size(ord_trace_norm,2)
+%         p = patch([times(jj),times(jj)+1,times(jj)+1,times(jj)],[ii-1, ii-1, ii, ii],ord_trace(ii,jj));
+%         set(p,'FaceColor','flat','EdgeColor','none');
+%         
+%     end
+% end
+% title('rGecko Intensity (AU)','FontSize',16)
+% xlabel('Frame','FontSize',16);
+% yticks(1:size(ord_trace,1));
+% yticklabels(num2str(round(spanval(ordx),2)'))
+% ylabel('Span Value (pixels)','FontSize',16)
+% xlim([0 size(ord_trace,2)]);
+% ylim([0 size(ord_trace,1)]);
+% c = colorbar;
+% close(h);
+%% write to excel file
+% save max extents
+xlswrite(excelfileinfo_maxextent,maxsizes',sheetname);
+% save F values
+xlswrite(excelfileinfo_Fvalues,ord_trace,sheetname);
+%% save some of the file info
+save(fullfile(pathname,[filename(1:end-4) '_analysisinfo']),'threshold_parameter','ord_trace','trace_raw','ordx','sumlbl','maxsizes','lbl','tt');
+% saveas(g,fullfile(pathname,[filename(1:end-4) '_rawIntensityHeatMap']),'png');
+
