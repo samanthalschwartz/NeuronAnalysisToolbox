07d0f3b3426a17f016d3675fce613ed87d8629d9
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-08-24 07:18:12 -0600

    a

--- InProgress/calcium analysis/GcampSpineAnalysis.m
+++ InProgress/calcium analysis/GcampSpineAnalysis.m
@@ -29,55 +29,121 @@ post_firstframe = img_post(:,:,end);
 img_post_foralign = cat(3,pre_lastframe,post_firstframe);
 [out,sv_arr] = ga.timedriftCorrect(img_post_foralign);
 newimg_post = ga.applyshift2series(img_post,sv_arr);
-% --- delete
-% test = ga.applydriftCorrect(post_firstframe,sv_arr);
-% test1 = cat(3,pre_lastframe,test);
-% img_post_align = ga.applydriftCorrect(img_post,repmat(sv_arr,1,size(img_post,3)));
-% img_post = img_post_aligned(:,:,1:end);
-% --- delete
+
 fullseq = cat(3,img_pre,newimg_post);
  % align cellfill to img
 imgsum = sum(fullseq,[],3);
-
-test = ga.applyshift2series(img_post,sv_arr);
-% test = ga.applydriftCorrect(post_firstframe,sv_arr);
-test1m  = cat(3,pre_lastframe,test);
-img_post_align = ga.applydriftCorrect(img_post,repmat(sv_arr,1,size(img_post,3)));
-img_post = img_post_aligned(:,:,1:end);
-
-test = cat(3,img_pre,img_post);
-% align cellfill to img
-imgsum = sum(img,[],3);
-corrimg = cat(3,imgsum,cellfill_pre);
+corrimg = cat(3,sum(fullseq(:,:,end-5:end),[],3),cellfill_pre);
 dc_corrimg = ga.timedriftCorrect(corrimg);
 cellfill = dc_corrimg(:,:,end);
-
-%% try and find the calcium transients
-% if last frame is empty uncomment this line
-threshold_parameter = 1.5;
-ca = img;
-ca = ca(:,:,0:end-1);
-% first smooth image
-ca_g = gaussf(ca,[1 1 0]);
-% now normalize to median
-ca_gnorm = ca_g;%- medif(ca_g);
-% now take difference
-ca_dzz = dzz(ca_gnorm,[1 1 1]);
-% get absolute values
-ab_ca_dzz = abs(ca_dzz);
-gab_ca_dzz = gaussf(ab_ca_dzz,[1 1 0]);
-tt = threshold(ab_ca_dzz^threshold_parameter,'otsu');
-lbl = label(tt,1,1,10^10);
-% this is to view the identified masks over the image
-[h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(ca,bdilation(lbl>0,2));
-
-%% try another way
+%% identify calcium
+maxsz = 20;
+tic;
+ca = fullseq;
 thresh_param = 1;
+h = waitbar(0,'Identifying Calcium Transients (Finding Intensity Changes) ....');
+% h = msgbox('Thresholding Image (Identifying Calcium Transients)....','Thanks');
 imgg = gaussf(ca,[1 1 0]);
+waitbar(.2,h);
 diffimage = dzz(imgg);
+waitbar(.3,h,'Identifying Calcium Transients (Smoothing Data)....');
 adiff = abs(diffimage);
 lp = ga.imgLaplaceCutoff(adiff,[1 1 0],[1 1 0]); %this helps smooth image (especially enhancing round objects)
+waitbar(.4,h);
 lp_ = lp.^thresh_param;
+waitbar(.5,h,'Identifying Calcium Transients (Thresholding)....');
 tt = threshold(lp_,'otsu');
 lb = label(tt);
-[h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(ca,bdilation(lb>0,2));
+waitbar(.7,h,'Identifying Calcium Transients (Measuring)....');
+msr = measure(tt,ca,{'size','DimensionsCube'});
+spanval = max(msr.DimensionsCube([1 2],:));
+% remove labels that are too big
+largemasksbool = spanval>maxsz;
+largemasksID = find(largemasksbool);
+slb = single(lb);
+for ii = 1:numel(largemasksID)
+slb(slb==largemasksID(ii)) = 0;
+end
+cleanlb = dip_image(slb);
+waitbar(1,h);
+close(h)
+toc;
+% underimgin = repmat(cellfill,1, 1, size(fullseq(:,:,500:end),3));
+% newmask =ga.cleanUpMask_manual_square(underimgin,cleanlb(:,:,500:end)>0);
+% newcleanlb = cat(3,cleanlb(:,:,0:949),newmask);
+% mask4overlay = bdilation(tt,2);
+% [h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(ca,mask4overlay);
+%%
+
+%% for each label, get intensity over the entire movie
+
+stt = sum(cleanlb,[],3);
+stt = stt>0;
+slb = label(stt);
+msrsum = measure(slb,cellfill,{'size','P2A'});
+longids = find(msrsum.P2A>0.7);
+sslb = single(slb);
+for ii = 1:numel(longids)
+    sslb(sslb==longids(ii)) = 0;
+end
+cleaner = dip_image(sslb);
+
+mask2clean = cleaner>0;
+cleanslb =ga.cleanUpMask_manual_square(squeeze(cellfill),cleaner>0,200);
+
+tracelb= label(cleanslb);
+trace = zeros(max(tracelb),size(fullseq,3));
+traceraw = zeros(max(tracelb),size(fullseq,3));
+
+wb = waitbar(0,'Quantifying Calcium Change in ROIs...');
+for ll = 1:max(tracelb)%[1:8,10:max(lbl)]
+%     tic
+currmask = tracelb==ll;
+bcurrmask = bdilation(currmask);
+mask2use = repmat(bcurrmask,1,1,size(fullseq,3));
+sumval = sum(fullseq,mask2use,[1 2]);
+% imgINmask = image*mask2use;
+% sumval = sum(imgINmask,[],[1 2]);
+% sizeval = sum(bcurrmask,[],[1,2]);
+sumtrace = single(sumval);
+tracemed = median(sumtrace);
+trace(ll,:) = sumtrace./tracemed;
+traceraw(ll,:) = sumtrace;
+% toc
+waitbar(ll/max(tracelb),wb);
+end
+close(wb)
+
+allsum = sum(traceraw,2);
+[~, ordx] = sort(allsum, 'ascend');
+ord_trace = traceraw(ordx,:);
+% now plot
+h = msgbox('Plotting HeatMap....');
+times = 1:size(ord_trace,2);
+figure;
+% wb = waitbar(0,'Plotting some things...');
+% cnt = 0;
+for ii = 1:size(ord_trace,1)
+    for jj = 1:size(ord_trace,2)
+        p = patch([times(jj),times(jj)+1,times(jj)+1,times(jj)],[ii-1, ii-1, ii, ii],ord_trace(ii,jj));
+        set(p,'FaceColor','flat','EdgeColor','none');
+        
+    end
+end
+title('GCamp Intensity (AU)','FontSize',16)
+xlabel('Frame','FontSize',16);
+ylabel('Puncta #','FontSize',16)
+xlim([0 size(ord_trace,2)]);
+ylim([0 size(ord_trace,1)]);
+c = colorbar;
+close(h);
+
+values = cat(2,ordx,ord_trace);
+mat2write = num2cell(values);
+addrow = nan(1,size(mat2write,2));
+addrow = num2cell(addrow);
+addrow{1} = 'Label ID';
+addrow{2} = 'Absolute Calcium Intensity per frame';
+mat2write = [addrow;mat2write];
+ savefile = 'C:\Users\sammy\Documents\MATLAB\calciumtest.xlsx';
+ xlswrite(savefile,mat2write);
