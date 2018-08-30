3cf2b0c77e16bbbde81aeff474be88024fd75fe0
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-08-30 17:35:31 -0600

    updated with Hannah

--- InProgress/calcium analysis/GcampSpineAnalysis.m
+++ InProgress/calcium analysis/GcampSpineAnalysis.m
@@ -1,4 +1,4 @@
-%% load in files and all that (cell fill and align etc)
+%% select files and all that (cell fill and align etc)
 ga = GeneralAnalysis(); %make just an empty version of the class to be able to use ga as a shortcut
 % -- get pre file
 [FILENAME_pre, PATHNAME_pre] = uigetfile(fullfile(pwd,'*.*'),'Select a pre condition calcium image');
@@ -18,29 +18,30 @@ flstring_post=inputdlg(prompt_post,name,numlines,defaultanswer);
 flstring_post = flstring_post{1};
 % -- get cell fill file
 [FILENAME_cf, PATHNAME_cf] = uigetfile(fullfile(PATHNAME_post,'*.*'),'Select the corresponding cell fill image');
-
+%% load files
+% these are the raw data movies (they are dip images)
 img_pre = dip_image(ga.loadtiffseries(PATHNAME_pre,flstring_pre));
 img_post = dip_image(ga.loadtiffseries(PATHNAME_post,flstring_post));
 cellfill_pre = ga.loadtiff_1ch(fullfile(PATHNAME_cf,FILENAME_cf));
-
+%%
 % align post image with last frame of pre-image
 pre_lastframe = img_pre(:,:,end);
 post_firstframe = img_post(:,:,end);
 img_post_foralign = cat(3,pre_lastframe,post_firstframe);
 [out,sv_arr] = ga.timedriftCorrect(img_post_foralign);
 newimg_post = ga.applyshift2series(img_post,sv_arr);
-
+preframes = size(img_pre,3);
 fullseq = cat(3,img_pre,newimg_post);
  % align cellfill to img
 imgsum = sum(fullseq,[],3);
 corrimg = cat(3,sum(fullseq(:,:,end-5:end),[],3),cellfill_pre);
 dc_corrimg = ga.timedriftCorrect(corrimg);
 cellfill = dc_corrimg(:,:,end);
-%% identify calcium
-maxsz = 20;
+%% identify calcium transient - here is most of the interesting part
+maxsz = 10;
 tic;
 ca = fullseq;
-thresh_param = 1;
+thresh_param = 1; % setting to 1.5 makes less sensitive. setting to 0.7 makes more sensitive
 h = waitbar(0,'Identifying Calcium Transients (Finding Intensity Changes) ....');
 % h = msgbox('Thresholding Image (Identifying Calcium Transients)....','Thanks');
 imgg = gaussf(ca,[1 1 0]);
@@ -48,12 +49,16 @@ waitbar(.2,h);
 diffimage = dzz(imgg);
 waitbar(.3,h,'Identifying Calcium Transients (Smoothing Data)....');
 adiff = abs(diffimage);
-lp = ga.imgLaplaceCutoff(adiff,[1 1 0],[1 1 0]); %this helps smooth image (especially enhancing round objects)
+lp = ga.imgLaplaceCutoff(adiff,[1 1 1],[1 1 1]); %this helps smooth image (especially enhancing round objects)
 waitbar(.4,h);
 lp_ = lp.^thresh_param;
 waitbar(.5,h,'Identifying Calcium Transients (Thresholding)....');
+% test = adaptthresh(single(lp_));
+% BW = imbinarize(single(lp_),test);
+% test2 = threshold(single(lp_).*test,'otsu');
 tt = threshold(lp_,'otsu');
-lb = label(tt);
+%% 
+lb = label(tt,inf,2,10^10);
 waitbar(.7,h,'Identifying Calcium Transients (Measuring)....');
 msr = measure(tt,ca,{'size','DimensionsCube'});
 spanval = max(msr.DimensionsCube([1 2],:));
@@ -73,6 +78,8 @@ toc;
 % newcleanlb = cat(3,cleanlb(:,:,0:949),newmask);
 % mask4overlay = bdilation(tt,2);
 % [h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(ca,mask4overlay);
+[h,overlayim] = GeneralAnalysis.viewMaskOverlayPerimStatic(fullseq,bdilation(cleanlb>0,2));
+
 %%
 
 %% for each label, get intensity over the entire movie
@@ -80,8 +87,8 @@ toc;
 stt = sum(cleanlb,[],3);
 stt = stt>0;
 slb = label(stt);
-msrsum = measure(slb,cellfill,{'size','P2A'});
-longids = find(msrsum.P2A>0.7);
+msrsum = measure(slb,cellfill,{'size','P2A','DimensionsCube'});
+longids = find(msrsum.P2A>0.7 | msrsum.P2A<0.3); % maybe also 
 sslb = single(slb);
 for ii = 1:numel(longids)
     sslb(sslb==longids(ii)) = 0;
@@ -106,30 +113,38 @@ sumval = sum(fullseq,mask2use,[1 2]);
 % sumval = sum(imgINmask,[],[1 2]);
 % sizeval = sum(bcurrmask,[],[1,2]);
 sumtrace = single(sumval);
-tracemed = median(sumtrace);
-trace(ll,:) = sumtrace./tracemed;
+pre_median = median(sumtrace(:,:,1:preframes));
+post_median = median(sumtrace(:,:,preframes+1:end));
+pretrace = sumtrace(:,:,1:preframes)./pre_median;
+posttrace = sumtrace(:,:,preframes+1:end)./post_median;
+trace(ll,:) = squeeze(cat(3,pretrace,posttrace));
+% tracemed = median(sumtrace);
+% trace(ll,:) = sumtrace./tracemed;
 traceraw(ll,:) = sumtrace;
 % toc
 waitbar(ll/max(tracelb),wb);
 end
 close(wb)
-
-allsum = sum(traceraw,2);
+%%
+allsum = sum(trace,2);
 [~, ordx] = sort(allsum, 'ascend');
-ord_trace = traceraw(ordx,:);
+ord_trace = trace(ordx,:);
 % now plot
-h = msgbox('Plotting HeatMap....');
-times = 1:size(ord_trace,2);
-figure;
+h = msgbox('Plotting Hannah HeatMap (fast!)....');
+figure; hmap = heatmap(ord_trace)
+hmap.GridVisible = 'off'
+hmap.Colormap = jet(50);
+% times = 1:size(ord_trace,2);
+% figure;
 % wb = waitbar(0,'Plotting some things...');
 % cnt = 0;
-for ii = 1:size(ord_trace,1)
-    for jj = 1:size(ord_trace,2)
-        p = patch([times(jj),times(jj)+1,times(jj)+1,times(jj)],[ii-1, ii-1, ii, ii],ord_trace(ii,jj));
-        set(p,'FaceColor','flat','EdgeColor','none');
-        
-    end
-end
+% for ii = 1:size(ord_trace,1)
+%     for jj = 1:size(ord_trace,2)
+%         p = patch([times(jj),times(jj)+1,times(jj)+1,times(jj)],[ii-1, ii-1, ii, ii],ord_trace(ii,jj));
+%         set(p,'FaceColor','flat','EdgeColor','none');
+%         
+%     end
+% end
 title('GCamp Intensity (AU)','FontSize',16)
 xlabel('Frame','FontSize',16);
 ylabel('Puncta #','FontSize',16)
@@ -138,6 +153,11 @@ ylim([0 size(ord_trace,1)]);
 c = colorbar;
 close(h);
 
+savename = 'E:\Hannah Calcium\020\cell2ABnorm_heatmap';
+saveas(gcf,savename,'png');
+% saveas(gcf,savename,'fig');
+
+
 values = cat(2,ordx,ord_trace);
 mat2write = num2cell(values);
 addrow = nan(1,size(mat2write,2));
@@ -145,5 +165,12 @@ addrow = num2cell(addrow);
 addrow{1} = 'Label ID';
 addrow{2} = 'Absolute Calcium Intensity per frame';
 mat2write = [addrow;mat2write];
- savefile = 'C:\Users\sammy\Documents\MATLAB\calciumtest.xlsx';
- xlswrite(savefile,mat2write);
+savefile = 'E:\Hannah Calcium\020\cell2_control.xlsx';
+xlswrite(savefile,mat2write);
+
+%%
+% --- find the baseline
+% --- find any values > 2* baseline
+% --- check in local maximum in region of 6 (3 before, 3 before after)
+% --- avg the intensity per spine, and also # of 1s/frame before/after
+% ---
