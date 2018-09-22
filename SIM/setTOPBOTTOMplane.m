c223574a64519aa83c2ba0d4c537d33d73918a53
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2018-09-22 08:56:06 -0600

    a

--- SIM/setTOPBOTTOMplane.m
+++ SIM/setTOPBOTTOMplane.m
@@ -1,12 +1,16 @@
-% this script is for setting the top and bottom slice
+%% this script is for setting the top and bottom slice
+% set the file name
 filename = 'C:\Users\KennedyLab\Documents\Hannah\SIM data\SIM_files\PSD95ib488_gephyrin561_Abeta647_003_Reconstructed_SIM.mat';
 load(filename);
+% show the file to look through
 dipshow(s.ch1.image)
 GeneralAnalysis.viewMaskOverlay(s.ch1.image,s.ch1.mask);
 
-%%
+% set the planes
 obj.planeTOP = []; % -- set the top plane here
 obj.planeBOTTOM = []; % -- set the bottom plane here
+
+% save the file
 save(filename,'obj');
 
 
