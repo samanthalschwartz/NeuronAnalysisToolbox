7d00880d24b4299e45eb10d077ffd65944a4d270
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2019-03-22 12:43:37 -0600

    updated for both channels

--- microscope projecting/FRAP488_561_maxprojfiles.m
+++ microscope projecting/FRAP488_561_maxprojfiles.m
@@ -1,8 +1,8 @@
 close all; clear all
 datadir = 'G:\FromMicroscopeComputer\190316 pHujiIntensityTesting\FRAP\488FRAP\cell1_488FRAP_pHujiGaba_Cry2OligGephIB__20190316_84852 PM';
 savedir = datadir;
-
-ch488_filebase = '*_w0000_z*';
+%%
+ch488_filebase = 'cell1_488FRAP_pHujiGaba_Cry2OligGephIB__w0000_z*';
 ch488files = dir2cell(datadir,ch488_filebase);
 clear im_488;
 im_488 = [];
@@ -11,6 +11,17 @@ image = loadtiff(ch488files{ii});
 im_488 = cat(4,im_488,image);
 end
 image_488 = max(dip_image(im_488),[],4);
-
 GeneralAnalysis.LibTiff(image_488,fullfile(savedir,ch488_filebase(1:end-1)));
+%%
+ch561_filebase = 'cell1_488FRAP_pHujiGaba_Cry2OligGephIB__w0001_z*';
+ch561files = dir2cell(datadir,ch561_filebase);
+clear im_561;
+im_561 = [];
+for ii = 1:numel(ch561files)
+image = loadtiff(ch561files{ii});
+im_561 = cat(4,im_561,image);
+end
+image_561 = max(dip_image(im_561),[],4);
+
+GeneralAnalysis.LibTiff(image_561,fullfile(savedir,ch561_filebase(1:end-1)));
 
