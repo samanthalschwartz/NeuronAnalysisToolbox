93d2c173a4d9a88af665503ae3ae71da30d1509e
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2019-03-25 19:31:36 -0600

    added vectarrow.m

--- SIM/s190325.m
+++ SIM/s190325.m
@@ -8,10 +8,10 @@ obj.calculateNumberDensityCOM(0,1);
 obj.save;
 end
 % 
-% figure; hold on;
-% plot(obj.ch1.results.bins,obj.ch1.results.numabeta./obj.ch1.results.volume,'Color',[0 0 1])
-% plot(obj.ch1.abetaSIM.results.bins,obj.ch1.abetaSIM.results.numabeta./obj.ch1.abetaSIM.results.volume,'Color',[1 0 0])
-% 
-% figure; hold on;
-% plot(obj.ch2.results.bins,obj.ch2.results.numabeta./obj.ch2.results.volume)
-% plot(obj.ch2.abetaSIM.results.bins,obj.ch2.abetaSIM.results.numabeta./obj.ch2.abetaSIM.results.volume)
+figure; hold on;
+plot(obj.ch1.results.bins,obj.ch1.results.numabeta./obj.ch1.results.volume,'Color',[0 0 1])
+plot(obj.ch1.abetaSIM.results.bins,obj.ch1.abetaSIM.results.numabeta./obj.ch1.abetaSIM.results.volume,'Color',[1 0 0])
+
+figure; hold on;
+plot(obj.ch2.results.bins,obj.ch2.results.numabeta./obj.ch2.results.volume)
+plot(obj.ch2.abetaSIM.results.bins,obj.ch2.abetaSIM.results.numabeta./obj.ch2.abetaSIM.results.volume)
