54eccdd3bcfc5769ac787e20ccb9a27f424c5e57
ss <samantha.schwartz@ucdenver.edu>
2019-03-26 20:11:21 -0600

    a

--- SIM/vectordotproduct.m
+++ SIM/vectordotproduct.m
@@ -2,7 +2,6 @@ function [Zdist, rdist] = vectordotproduct(L1,L2,p1)
 % L1 pre-synaptic marker
 % L2 post-synaptic marker
 % p1 - abeta point
-
 lvec = L2-L1;
 pvec = p1-L1;
 Zdist = dot(lvec,pvec)/sqrt(sum(lvec.^2));
