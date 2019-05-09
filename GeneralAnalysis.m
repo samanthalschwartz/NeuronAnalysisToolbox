3ed3a26b5474d3b9a60b392a65722ffda8256142
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2019-05-09 07:24:42 -0600

    updated to have clean up mask for all frames function in GeneralAnalysis.m

--- GeneralAnalysis.m
+++ GeneralAnalysis.m
@@ -412,6 +412,72 @@ methods (Static)
       dipfig -unlink
       newmask = logical(lb);  
     end
+    
+    function newmask = cleanUpMask_manual_allframes(underimgin,mask_in,imviewsz)
+        %        lb = label(mask_in);
+%         ov = overlay(underimgin,mask_in);
+%         h = dipshow(ov,'log');
+%         dipmapping(h,'global')
+        %       h = dipshow(lb,'labels');
+%         while(ishandle(h))
+%             [a b] = dipcrop(h);
+%             mask_in(b(1,1):b(1,1)+b(2,1),b(1,2):b(1,2)+b(2,2),:) = 0;
+%             close(h);
+%             ov = overlay(underimgin,mask_in);
+%             h = dipshow(ov,'log');
+%             dipmapping(h,'global');
+%         end
+         if nargin<3
+             imviewsz = 150;
+         end
+        lb = label(logical(mask_in));
+        ov = underimgin;
+        ov(lb~=0) = 0;
+        g = dipfig('ov');
+        try
+        dipshow(ov,'log');
+        catch
+            dipshow(ov,'percentile');
+        end
+        diptruesize(g,imviewsz);
+        clmp = bone(255);
+        clmp(1,:) = [1 0 0];
+        while(ishandle(g))
+            try
+                [B,C] = dipcrop(g);
+%                 v = dipgetcoords(g,1);
+            catch
+                break;
+            end
+            gcfinfo = get(g,'UserData');
+            if ndims(B)==3
+                
+                 img2remove = lb(C(1,1):C(1,1)+C(2,1),C(1,2):C(1,2)+C(2,2),:);
+                 lbs2remove = unique(single(img2remove));
+                 
+            elseif ismatrix(B)
+                 img2remove = lb(C(1,1):C(1,1)+C(2,1),C(1,2):C(1,2)+C(2,2));
+                 lbs2remove = unique(single(img2remove));
+            end
+            for ii = lbs2remove(lbs2remove~=0)'
+            lb(lb == ii) = 0; 
+            end
+            ov = underimgin;
+            ov(lb~=0) = 0
+            diptruesize(gcf,imviewsz);
+            try
+            dipmapping('log')
+            catch
+                dipmapping('percentile')
+            end
+            dipmapping('colormap',clmp);
+        end
+      dipfig -unlink
+      newmask = logical(lb);  
+    end
+    
+    
+    
     function newmask = cleanUpMaskKeepers_manual_square(underimgin,mask_in,imviewsz)
         if nargin<3
             imviewsz = 50;
