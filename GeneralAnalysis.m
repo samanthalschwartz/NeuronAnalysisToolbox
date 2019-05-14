68e9a126b52fdd8a0860d088bfc3540a5f59d1cf
Samantha Schwartz <samantha.schwartz@ucdenver.edu>
2019-05-14 14:34:41 -0600

    FIXED update per frame

--- GeneralAnalysis.m
+++ GeneralAnalysis.m
@@ -412,6 +412,63 @@ methods (Static)
       dipfig -unlink
       newmask = logical(lb);  
     end
+    function newmask = cleanUpMask_byframe_square(underimgin,mask_in,imviewsz)
+        %        lb = label(mask_in);
+%         ov = overlay(underimgin,mask_in);
+%         h = dipshow(ov,'log');
+%         dipmapping(h,'global')
+        %       h = dipshow(lb,'labels');
+%         while(ishandle(h))
+%             [a b] = dipcrop(h);
+%             mask_in(b(1,1):b(1,1)+b(2,1),b(1,2):b(1,2)+b(2,2),:) = 0;
+%             close(h);
+%             ov = overlay(underimgin,mask_in);
+%             h = dipshow(ov,'log');
+%             dipmapping(h,'global');
+%         end
+         if nargin<3
+             imviewsz = 150;
+         end
+         maskin = dip_image(mask_in);
+        lb = label(logical(maskin));
+        ov = underimgin;
+        ov(lb~=0) = 0;
+        g = dipfig('ov');
+        try
+        dipshow(ov,'log');
+        catch
+            dipshow(ov,'percentile');
+        end
+        diptruesize(g,imviewsz);
+        clmp = bone(255);
+        clmp(1,:) = [1 0 0];
+        while(ishandle(g))
+            try
+                [B,C] = dipcrop(g);
+%                 v = dipgetcoords(g,1);
+            catch
+                break;
+            end
+            gcfinfo = get(g,'UserData');
+            if ndims(B)==3
+                currtime = gcfinfo.curslice;
+                maskin(C(1,1):C(1,1)+C(2,1),C(1,2):C(1,2)+C(2,2),currtime) = 0;     
+            elseif ismatrix(B)
+                maskin(C(1,1):C(1,1)+C(2,1),C(1,2):C(1,2)+C(2,2)) = 0;  
+            end
+            ov = underimgin;
+            ov(maskin~=0) = 0
+            diptruesize(gcf,imviewsz);
+            try
+            dipmapping('log')
+            catch
+                dipmapping('percentile')
+            end
+            dipmapping('colormap',clmp);
+        end
+      dipfig -unlink
+      newmask = maskin;  
+    end
     function newmask = cleanUpMaskKeepers_manual_square(underimgin,mask_in,imviewsz)
         if nargin<3
             imviewsz = 50;
